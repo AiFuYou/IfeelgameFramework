7083ac30b370c1461ec3bf0bafa78a11b54d009e
suyongfu <suyongfu.ifeel@gmail.com>
2021-03-13 09:47:28 +0800

    upgrade unity

---
+++ IfeelgameFramework/Assets/IfeelgameFramework/Plugin/Editor/BuildPlayer/BuildPlayerAndroid.cs
@@ -0,0 +1,35 @@
+﻿using IfeelgameFramework.Core.Logger;
+
+namespace IfeelgameFramework.Plugin.Editor.BuildPlayer
+{
+    public class BuildPlayerAndroid
+    {
+        private static BuildPlayerAndroid _instance;
+        private static readonly object _insLock = new object();
+        /// <summary>
+        /// 实例化LocalStorageManager单例，并创建存储文件夹
+        /// </summary>
+        public static BuildPlayerAndroid Instance
+        {
+            get
+            {
+                lock (_insLock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new BuildPlayerAndroid();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        public void GenAndroidProj()
+        {
+            DebugEx.Log("AndroidStudio工程修改");
+            
+            //修改gradle文件
+        }
+    }
+}
---
+++ IfeelgameFramework/Assets/IfeelgameFramework/Plugin/Editor/BuildPlayer/BuildPlayerIOS.cs
@@ -0,0 +1,37 @@
+﻿using IfeelgameFramework.Core.Logger;
+
+namespace IfeelgameFramework.Plugin.Editor.BuildPlayer
+{
+    public class BuildPlayerIOS
+    {
+        private static BuildPlayerIOS _instance;
+        private static readonly object _insLock = new object();
+        /// <summary>
+        /// 实例化LocalStorageManager单例，并创建存储文件夹
+        /// </summary>
+        public static BuildPlayerIOS Instance
+        {
+            get
+            {
+                lock (_insLock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new BuildPlayerIOS();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        public void GenIOSProj()
+        {
+            DebugEx.Log("Xcode工程文件修改");
+            
+            //修改plist
+            
+            //修改project.pbxproj
+        }
+    }
+}
